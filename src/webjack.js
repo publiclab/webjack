2152097c212495e8f92dcedf61416816c10fc972
Richard Meister <richard.meister.de@gmail.com>
2016-05-30 23:18:08 +0200

    prepared code that records audio; added adapter.js (via Bower) for enhanced webRTC interop; changed jQuery into Bower dependency

--- src/webjack.js
+++ src/webjack.js
@@ -3,6 +3,43 @@ WebJack.Connection = Class.extend({
   init: function(args) {
 
     var connection = this;
+		var audioCtx = new AudioContext();
+
+		function onAudioProcess(event) {
+		  var buffer = event.inputBuffer;
+		  var samplesIn = buffer.getChannelData(0);
+		  console.log("-- audioprocess data (" + samplesIn.length + " samples) --");
+		  // TODO: decode
+		}
+
+		function successCallback(stream) {
+		  var audioTracks = stream.getAudioTracks();
+		  console.log('Using audio device: ' + audioTracks[0].label);
+		  console.log("-- samplerate (" + audioCtx.sampleRate + ") --");
+		  stream.onended = function() {
+		    console.log('Stream ended');
+		  };
+		  audioSource = audioCtx.createMediaStreamSource(stream);
+		  decoderNode = audioCtx.createScriptProcessor(8192, 1, 1); // buffersize, input channels, output channels
+		  audioSource.connect(decoderNode);
+		  decoderNode.addEventListener("audioprocess", onAudioProcess);
+		  decoderNode.connect(audioCtx.destination); // Chrome does not fire events without destination 
+		}
+
+		function errorCallback(error) {
+		  console.log('navigator.getUserMedia error: ', error);
+		}
+
+		navigator.mediaDevices.getUserMedia(
+			{
+			  audio: true,
+			  video: false
+			}
+		).then(
+		  successCallback,
+		  errorCallback
+		);
+
 
     connection.args = args; // connection.args.baud_rate, etc
 
