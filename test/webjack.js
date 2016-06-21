7c2a43aaaae505b822e700abfb59b6d0628115b0
jywarren <jeff@unterbahn.com>
2016-06-21 08:24:02 -0400

    basic empty test suite in tape

---
+++ test/webjack.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var fs = require('fs');
+var test = require('tape');
+var webjacek = require('..');
+
+function read (file) {
+  return fs.readFileSync('./test/fixtures/' + file, 'utf8').trim();
+}
+
+function write (file, data) { /* jshint ignore:line */
+  return fs.writeFileSync('./test/fixtures/' + file, data + '\n', 'utf8');
+}
+
+test('webjack has tests', function (t) {
+  // read('something.html')
+  t.equal(true, true);
+  t.end();
+});
