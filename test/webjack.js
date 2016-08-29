5a31fa18a69006d81a0a017f0c49a0dbf48d43eb
jywarren <jeff@unterbahn.com>
2016-08-29 17:18:34 -0400

    stubbed failing test

--- test/webjack.js
+++ test/webjack.js
@@ -41,3 +41,14 @@ test('webjack module is exported', function (t) {
   t.equal(typeof conn === 'object', true);
   t.end();
 });
+
+test('webjack constructor sets options via profiles, exposes via public .options api', function (t) {
+  var conn = new webjack.Connection(webjack.profiles.Browser);
+  t.equal(typeof conn === 'object', true);
+  t.equal(conn.options.baud, webjack.profiles.Browser.baud);
+  t.equal(conn.options.freqLow, webjack.profiles.Browser.freqLow);
+  t.equal(conn.options.freqHigh, webjack.profiles.Browser.freqHigh);
+  t.equal(conn.options.echoCancellation, webjack.profiles.Browser.echoCancellation);
+  t.equal(conn.options.softmodem, webjack.profiles.Browser.softmodem);
+  t.end();
+});
