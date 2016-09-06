726bfd01e22b8cdb4c484be92fb14c82704aaeb9
jywarren <jeff@unterbahn.com>
2016-09-06 13:23:10 -0400

    adapted test

--- test/webjack.js
+++ test/webjack.js
@@ -43,12 +43,15 @@ test('webjack module is exported', function (t) {
 });
 
 test('webjack constructor sets options via profiles, exposes via public .options api', function (t) {
-  var conn = new webjack.Connection(webjack.profiles.Browser);
+  var profile = webjack.Profiles.Browser;
+  profile.audioCtx = AudioContext;  // mocking audiocontext for node
+  profile.navigator = navigator;    // same for navigator
+  var conn = new webjack.Connection(profile);
   t.equal(typeof conn === 'object', true);
-  t.equal(conn.options.baud, webjack.profiles.Browser.baud);
-  t.equal(conn.options.freqLow, webjack.profiles.Browser.freqLow);
-  t.equal(conn.options.freqHigh, webjack.profiles.Browser.freqHigh);
-  t.equal(conn.options.echoCancellation, webjack.profiles.Browser.echoCancellation);
-  t.equal(conn.options.softmodem, webjack.profiles.Browser.softmodem);
+  t.equal(conn.options.baud, webjack.Profiles.Browser.baud);
+  t.equal(conn.options.freqLow, webjack.Profiles.Browser.freqLow);
+  t.equal(conn.options.freqHigh, webjack.Profiles.Browser.freqHigh);
+  t.equal(conn.options.echoCancellation, webjack.Profiles.Browser.echoCancellation);
+  t.equal(conn.options.softmodem, webjack.Profiles.Browser.softmodem);
   t.end();
 });
