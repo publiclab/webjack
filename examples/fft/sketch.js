7574e57a43af19c2d0d55db5a611034db65c0168
jywarren <jeff@unterbahn.com>
2019-03-28 17:34:11 -0400

    add click interaction to get past security limits

--- examples/fft/sketch.js
+++ examples/fft/sketch.js
@@ -5,12 +5,17 @@
  * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
  * and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em></p>
  */
-var mic, fft, labelScale = 3;
+var mic, fft,
+  labelScale = 3, 
+  userInteracted = false; // need to wait for user interaction before using WebAudio https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio
 
-function setup() {
-  createCanvas(400, 100);
+function setupEqualizer() {
+  createCanvas(350, 100);
   noFill();
   $('.equalizer').append($('.p5Canvas').remove()) 
+  $('.eq-btn').hide() 
+
+  userInteracted = true;
 
   mic = new p5.AudioIn();
   mic.start();
@@ -19,26 +24,27 @@ function setup() {
 }
 
 function draw() {
-  var scale = 1;
-  background(200);
-
-  text(parseInt(map(mouseX, 0, width, 20, 15000 / scale)) + " Hz", mouseX - 10, mouseY - 20);
-  var spectrum = fft.analyze();
-
-  beginShape();
-  vertex(0, height * 0.9);
-  for (i = 0; i < spectrum.length / scale; i++) {
-    vertex(i * scale, map(spectrum[i], 0, 255, height * 0.8, 0));
+  if (userInteracted) {
+    var scale = 1;
+    background(200);
+ 
+    text(parseInt(map(mouseX, 0, width, 20, 15000 / scale)) + " Hz", mouseX - 10, mouseY - 20);
+    var spectrum = fft.analyze();
+ 
+    beginShape();
+    vertex(0, height * 0.9);
+    for (i = 0; i < spectrum.length / scale; i++) {
+      vertex(i * scale, map(spectrum[i], 0, 255, height * 0.8, 0));
+    }
+    vertex(width, height * 0.9);
+    endShape();
+ 
+    var freq
+    // draw labels
+    for (i = 0; i < spectrum.length / scale; i += width / labelScale) {
+      freq = parseInt(map(i, 0, 1024, 20, 15000));
+      text(freq, map(i, 0, 1024 / scale, 0, width), height * 0.97);
+      fill(0, 100);
+    }
   }
-  vertex(width, height * 0.9);
-  endShape();
-
-  var freq
-  // draw labels
-  for (i = 0; i < spectrum.length / scale; i += width / labelScale) {
-    freq = parseInt(map(i, 0, 1024, 20, 15000));
-    text(freq, map(i, 0, 1024 / scale, 0, width), height * 0.97);
-    fill(0, 100);
-  }
-
 }
